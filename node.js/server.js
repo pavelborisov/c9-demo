bb1697d61f581261fe75670ef9df55dd54a8b233
OpenShift guest <kshadow@list.ru>
2013-09-21 10:22:54 -0400

    add templating engine EJS

--- node.js/server.js
+++ node.js/server.js
@@ -1,11 +1,34 @@
-// reference the http module so we can create a webserver
-var http = require("http");
+var express = require("express")
+var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
-// create a server
-http.createServer(function(req, res) {
-    // on every request, we'll output 'Hello world'
-    res.end("Hello world from Cloud9!");
-}).listen(process.env.PORT, process.env.IP);
+require("./user.js")
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+app.set('views', __dirname + '/layout');
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+app.set('layout', 'layout')
+
+app.use(expressLayouts)
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'The index page!', users: users })
+});
+
+app.listen(process.env.PORT, process.env.IP);
 
 // Note: when spawning a server on Cloud9 IDE, 
 // listen on the process.env.PORT and process.env.IP environment variables
